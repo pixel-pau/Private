e26c3da8dee7cfb52245bbd40c65449f07aa98e6
pixel-pau <kullancanmi58@gmail.com>
2026-05-10 14:09:23 +0300

    Update Kill.lua

--- Kill.lua
+++ Kill.lua
@@ -1,21 +1,22 @@
-local player = game:GetService("Players").LocalPlayer
-local library = loadstring(game:HttpGet("https://raw.githubusercontent.com/SynioxStudios/Syniox-Ui/refs/heads/main/Syniox%20Gui.txt"))()
-local displayName = player.DisplayName
+local library = loadstring(game:HttpGet("https://raw.githubusercontent.com/SynioxStudios/Syniox-Ui/refs/heads/main/Syniox%20Gui.txt", true))()
+local displayName = game.Players.LocalPlayer.DisplayName
 
-local window = library:AddWindow("YuSuF | Private Kill || Hİ Sir - ".. displayName, {
+local window = library:AddWindow("YuSuF | Private Kill || Hİ SİR - ".. displayName, {
     title_bar = {
-        Color3.fromRGB(150, 0, 0),
-        Color3.fromRGB(60, 0, 0),
+        Color3.fromRGB(160, 0, 0), 
+        Color3.fromRGB(40, 0, 0),  
+        Color3.fromRGB(0, 0, 0)    
+    }, 
+    title_bar_transparency = 0.1, 
+    background = {
+        Color3.fromRGB(0, 0, 0), 
+        Color3.fromRGB(10, 10, 10), 
         Color3.fromRGB(20, 20, 20)
     }, 
-    title_bar_transparency = 0.05, 
-    background = {
-        Color3.fromRGB(15, 10, 10),
-        Color3.fromRGB(25, 15, 15),
-        Color3.fromRGB(0, 0, 0)
-    }, 
     background_transparency = 0.1, 
-    main_color = Color3.fromRGB(200, 0, 0),
-    min_size = Vector2.new(450, 320), 
+    main_color = Color3.fromRGB(160, 0, 0), 
+    min_size = Vector2.new(450, 330), 
     can_resize = true 
 })
+
+local kill = window:AddTab("Kill")
